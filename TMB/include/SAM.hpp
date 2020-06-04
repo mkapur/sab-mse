f04b436957af43f147561e0a403220e677f101e1
Maia Sosa Kapur <kapurm@uw.edu>
2020-06-04 12:12:56 -0700

    port in hake cpp and add nspace

---
+++ TMB/include/SAM.hpp
@@ -0,0 +1,8 @@
+#include "define.hpp"
+#include "derived.hpp"
+#include "f.hpp"
+#include "predn.hpp"
+#include "n.hpp"
+#include "predobs.hpp"
+#include "obs.hpp"
+// these have to proceed in this order

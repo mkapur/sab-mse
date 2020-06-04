fc4844702a801a619f6bd82b0c97c6d2c4d9fe35
Maia Sosa Kapur <kapurm@uw.edu>
2020-06-04 12:26:51 -0700

    rm conflicted copies

--- R/OM_master.R
+++ R/OM_master.R
@@ -1,112 +1,174 @@
 ## OM_Master.R
-## M S KAPUR 
-## Generate data, condition and export based on OM config, calling functions.R and TMB model XX
+## M S Kapur mod N Jacobsen Summer 2020
+## kapurm@uw.edu
+
+library(TMB)
+library(dplyr)
+library(reshape2)
+library(ggplot2)
+library(r4ss)
+library(here)
+library(ggsidekick)
+source(here("R","functions",'load_files_OM.R'))
+compile(here("TMB","runsabassessment.cpp"))
+dyn.load(dynlib(here("TMB","runsabassessment")))
+
+## OM MODEL INIT ----
+# Initialize the model parameters. Make a version with movement and no seasons (simple)
+df.simple <- load_data_seasons(nseason = 1, 
+                               nspace = 2, 
+                               bfuture = 0.5, 
+                               movemaxinit = 0.5, 
+                               movefiftyinit =8) # Prepare data for operating model
+# Run the model using 'run.agebased.true.catch()' -- will fail if pars not == nspace
+sim.data.simple <- run.agebased.true.catch(df.simple)
+
+## sanity checks
+df <- sim.data.simple$SSB %>% 
+  data.frame() %>%
+  mutate('totalSSB' = X1+X2, year = as.numeric(row.names(.))) %>%
+  melt(id = 'year') 
+  ggplot(df, aes(x = year, y = value, color = variable)) + 
+  geom_line(size =2 ) +  theme_sleek()
+  
+## OM MODEL CONDITIONING ----
+  # runomem and run om condition
+set.seed(731)
+plot.figures = FALSE # Set true for printing to file 
+# Run the simulation model
+assessment <- read.csv(here("input","data",'assessment_MLE.csv')) ## I believe this comes from SS3
+assessment <- assessment[assessment$year > 1965 &assessment$year < 2018 ,]
+Catch.obs <- read.csv(here("input","data",'sab_totcatch.csv'))
+df <- load_data_seasons()
+df$Catch <- Catch.obs$Fishery
+time <- 1
+yrinit <- df$nyear
+### Run the OM and the EM for x number of years in the MSE 
+### Set targets for harvesting etc 
+# df$parms$initN <- df$parms$initN*0
+# df$parms$Rin <- df$parms$Rin*0
+# df$F0 <- 0*df$F0
+simyears <- 25 # Project 30 years into the future (2048 that year)
+year.future <- c(df$years,(df$years[length(df$years)]+1):(df$years[length(df$years)]+simyears))
+N0 <- NA
+sim.data <- run.agebased.true.catch(df)
+simdata0 <- sim.data # The other one is gonna get overwritten. 
+  
 
 
 
-## Load packages and functions
-source('./R/presets.R') 
-mapply(source, list.files("./R/functions", pattern = ".R", full.names=TRUE))
+# Plott stuff 
 
-## Load real data and pass to build_dat, build_pars to get in TMB-friendly format
-surv <- read.csv("input/cleaned/clean_survey.csv")
-NsurveyFleets <- length(unique(surv$fleet))
+parms <- getParameters_OM(trueparms = TRUE, df = df)
 
-landings <- read.csv("./input/cleaned/clean_landings.csv") ## made in dataprep.R
-discard <- read.csv("./input/cleaned/clean_discard.csv") ## made in dataprep.R
-NfisheryFleets <- length(unique(landings$fleet))
+##  Create a data frame to send to runsabassessment 
 
+df.new <- create_TMB_data(sim.data, df, history = TRUE)
 
-agecomps <-  read.csv("./input/cleaned/clean_agecomps.csv") ## made in dataprep.R
-lencomps <-  read.csv("./input/cleaned/clean_lencomps.csv") ## made in dataprep.R
+parms.new <- parms
+F0 <- rowSums(sim.data$Fout)
+Rdev <- parms$Rin
+parms.new$F0 <- F0
+parms.new$Rin <- Rdev
 
-NFleets <- sum(NsurveyFleets,NfisheryFleets)
+obj <- MakeADFun(df.new,
+                 parms.new,
+                 DLL= "runsabassessment") # Run the assessment, in TMB folder
+reps <- obj$report()
 
-## Structure
-# Nareas <- 7 ## number of modeled areas
-# startyr <- min(ts$year)               
-# endyr <- YEAR <- max(ts$year)           
-# nyears <- length(syr:lyr)               
-# rec_age <- min(waa$age)               # recruitment age                  
-# plus_group <- max(waa$age)            # plus group age
-# nage <- length(rec_age:plus_group)    # number of ages
-# nlenbin <- length(unique(len$length_bin)) # number of length bins - poss vary by area
-# nsex <- 2                 # single sex or sex-structured
-# nproj <- 1                # projection years *FLAG* eventually add to cpp file, currently just for graphics
-# include_discards <- TRUE  # include discard mortality, TRUE or FALSE
-# tmp_debug <- TRUE         # Temporary debug flag, shut off estimation of selectivity pars
-# 
-# # Model switches
-# rec_type <- 1     # Recruitment: 0 = penalized likelihood (fixed sigma_r), 1 = random effects
-# slx_type <- 1     # Selectivity: 0 = a50, a95 logistic; 1 = a50, slope logistic
-# comp_type <- 0    # Age comp likelihood (not currently developed for len comps): 0 = multinomial, 1 = Dirichlet-multinomial
-# spr_rec_type <- 1 # SPR equilbrium recruitment: 0 = arithmetic mean, 1 = geometric mean, 2 = median (not coded yet)
-# M_type <- 0       # Natural mortality: 0 = fixed, 1 = estimated with a prior
+# SSB 
+plot(df$years,rowSums(sim.data$SSB.weight))
+lines(df$years,SSB.ss3*0.5)
+lines(df$years,reps$SSB, col = 'red')
+# Survey 
+plot(df$years,sim.data$survey.true, type ='l')
+points(df$years[df$survey_x == 2],df$survey[df$survey_x == 2,])
+points(df$years[df$survey_x == 2],df.new$survey[df$survey_x == 2], col = 'red')
+points(df$years[df$survey_x == 2],reps$Surveyobs[df$survey_x == 2], col = 'green')
 
 
-## Create dataframes, similar to VAST 
-# data <- makeDat()
+lower <- obj$par-Inf
+lower[names(lower) == 'F0'] <- 0.001
+upper <- obj$par+Inf
+upper[names(upper) == 'PSEL'] <- 9
+upper[names(upper) == 'logphi_catch'] <- log(15)
+upper[names(upper) == 'logh'] <- log(0.999)
+upper[names(upper) == 'F0'] <- 2
+upper[names(upper) == 'psel_fish'] <- 3
+lower[names(lower) == 'psel_fish'] <- 0.0001
 
-## these are sitting in input; the function is source()d in helpers.R
-load("input/temp/cc_catch_2020-04-17.Rdata") ## loads as cn  ## list by fleet with single column for catches
-cw <- read.ices("./input/ices_testing/cw.dat") ## catch weights by fleet-year
-dw <- read.ices("./input/ices_testing/dw.dat") ## discard weights by fleet-year
-lf <- read.ices("./input/ices_testing/lf.dat") 
-lw <- read.ices("./input/ices_testing/lw.dat")
-mo <- read.ices("./input/ices_testing/mo.dat")
-nm <- read.ices("./input/ices_testing/nm.dat")
-pf <- read.ices("./input/ices_testing/pf.dat")
-pm <- read.ices("./input/ices_testing/pm.dat")
-sw <- read.ices("./input/ices_testing/sw.dat")
-surveys <- load("input/temp/cc_surv_2020-04-17.Rdata") #read.ices("./input/ices_testing/survey.dat")
+## optimize!
+system.time(opt<-nlminb(obj$par,obj$fn,obj$gr,lower=lower,upper=upper,
+                        control = list(iter.max = 1e6, 
+                                       eval.max = 1e6))) 
 
+rep <- sdreport(obj)
+sdrep <- summary(rep)
+rep.values<-rownames(sdrep)
+nyear <- df$tEnd
 
+R <- data.frame(name = sdrep[rep.values == 'R',1])
 
-## format data 
-dat2 <- makeDat(fleets = cn,
-                surveys=surveys)
-                      # prop.mature=mo, 
-                      # stock.mean.weight=sw, 
-                      # catch.mean.weight=cw, 
-                      # dis.mean.weight=dw, 
-                      # land.mean.weight=lw,
-                      # prop.f=pf, 
-                      # prop.m=pm, 
-                      # natural.mortality=nm, 
-                      # land.frac=lf)
+SSB <- data.frame(name = sdrep[rep.values == 'SSB',1])
+SSB$SE <- sdrep[rep.values == 'SSB',2]
+SSB$min <- SSB$name-2*SSB$SE
+SSB$max <- SSB$name+2*SSB$SE
+SSB$year <- df$years
 
-## create basic config
-conf <- modConfig(dat)
-## overwrite as needed
-conf$stockRecruitmentModelCode <- 2 ## BH
+Catch <- getUncertainty('Catch', df.new, sdrep)
+SSB <- getUncertainty('SSB',df.new, sdrep)
 
-conf$fbarRange <- c(2,6)
-conf$corFlag <- 1
-conf$keyLogFpar <- matrix(c(
-  -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,
-  -1,    0,    1,    2,    3,    4,    5,    6,   -1,
-  -1,    7,   -1,   -1,   -1,   -1,   -1,   -1,   -1,
-  8,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1), nrow=4, byrow=TRUE)
-
-par <- makePar(dat,conf)
-# These default initial can be modified (like the configuration)
-par$logFpar <- rep(0,9)
+plot(SSB$value)
+lines(rowSums(sim.data$SSB))
+lines(SSB.ss3*0.5, col = 'red')
 
 
-## only need to do this part if CPP has changed
-compile("./TMB/CopyOfstockassessment.cpp")
-dyn.load(dynlib("./TMB/CopyOfstockassessment"))
+# Compare the estimated parameters 
+df.p <- as.data.frame(rep$par.fixed)
+df.p$name <- names(rep$par.fixed)
+df.p$idx <- 1:nrow(df.p)
+df.p$model <- 'TMB'
+names(df.p)[1] <- 'parameter'
+df.p2 <- as.data.frame(unlist(parms.new))
+df.p2$name <- names(rep$par.fixed)
 
-# Now we are ready to optimize the model. This function runs the TMB call
-fit <- runMod(dat2,conf,par) ## currently calling CopyOfstockassessment
+df.p2$idx <- 1:nrow(df.p2)
+names(df.p2)[1] <- 'parameter'
+df.p2$model <- 'SS3'
+df.plot <- rbind(df.p,df.p2)
 
-stockassessment:::ssbplot(fit) ## this is a stockassessment function; likely rewrite; will fail class(fit) != 'sam'
+# Fix the log values 
+idx <- grep('log', df.plot$name)
+df.plot$parameter[idx] <- exp(df.plot$parameter[idx])
+
+ggplot(df.plot, aes(x=  idx, y = parameter, color = model))+geom_point()+
+  facet_wrap(~name,scales = 'free')+theme_classic()
 
 
-## DEPRECATED ----
-# parameters <- build_parameters()
-# random_vars <- build_random_vars()
-# 
-# TMBphase(data, parameters, random = random_vars, 
-#          model_name = "mod", phase = FALSE, 
-#          debug = FALSE)
+ss.exp <- rep(NA, df$nyear)
+SSB.ss3 <- mod$derived_quants$Value[grep('SSB_1966', mod$derived_quants$Label):grep('SSB_2018', mod$derived_quants$Label)]
+
+
+df.ss <- data.frame(year = rep(df$years,3), 
+                    SSB = c(SSB$value,
+                            reps$SSB,
+                            SSB.ss3*0.5
+                    ),
+                    model = rep(c('TMB est','TMB SS3 parms','SS3'), each = df$tEnd)
+)
+
+cols <- PNWColors::pnw_palette('Starfish', n = length(unique(df.ss$model)))
+
+p.ssb <- ggplot(df.ss[df.ss$model != 'Obs' & df.ss$model != 'SS3',], aes(x = year, y = SSB*1e-6, color = model))+
+  geom_line()+
+  geom_point(data = df.ss[df.ss$model %in% c('SS3','Obs'),])+
+  theme_classic()+scale_y_continuous('Spawning biomass\n (million tonnes)')+
+  scale_color_manual(values = cols)+
+  theme(legend.position = c(0.8,0.8),
+        legend.title = element_blank())+
+  geom_ribbon(data = SSB, aes(ymin = min*1e-6, ymax = max*1e-6, x = year,
+                              y= value*1e-6, color = NA, group = NA),
+              fill = alpha('gray', alpha = 0.3), color = NA)
+
+p.ssb
+

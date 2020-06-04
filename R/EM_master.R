c877dd5fb1a167ac7b5b3788f6db4538b11109bc
Maia Sosa Kapur <kapurm@uw.edu>
2020-06-04 12:23:16 -0700

    ignore R for now

--- R/EM_master.R
+++
@@ -1,3 +0,0 @@
-## EM_Master.R
-## M S KAPUR 
-## Generate data, condition and export based on EM spatial and HCR config, calling functions.R and TMB model XX
--- R/MSE_master.R
+++
@@ -1,3 +0,0 @@
-## MSE_Master.R
-## M S KAPUR 
-## call om and ems in loops if necessary, and post process and generate plots as needed

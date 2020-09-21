c072f5cded0a14b12624375472a39dce305c2391
Maia Sosa Kapur <kapurm@uw.edu>
2020-09-21 15:40:12 -0700

    Update plotChecks.R

--- R/functions/plotChecks.R
+++ R/functions/plotChecks.R
@@ -14,6 +14,11 @@ pNinit <- Ninit_ais[,,1] %>%
   labs(x = 'Age in Initial Years',y = 'Initial Numbers', color = 'subarea') +
   ggsidekick::theme_sleek()
 
+ggsave(pNinit,
+       file = here('figs',
+                   "NZeroNinit_Move_BothSex_normalizedOmega.png"),
+       width = 6, height = 4, unit = 'in',
+       dpi = 420)
 
 pNzeroF <- N_0ais[,,1] %>% data.frame() %>%
   mutate('Age' = 1:71) %>%
@@ -34,7 +39,7 @@ pNzeroM <- N_0ais[,,2] %>% data.frame() %>%
 
 ggsave(pNzeroF  | pNzeroM,
        file = here('figs',
-                   "NZero_Move_2Sex.png"),
+                   "NZero_Move_2Sex_normalizedOmega.png"),
        width = 6, height = 4, unit = 'in',
        dpi = 420)
 
@@ -46,27 +51,36 @@ for(i in 1:6){
   plot(N_yais_beg[1,,i,1],
        type = 'l',
        lwd = 2, 
-       col = subareaPal[i],
+
+       col = 'black',
        main = inames[i], 
        col.main =subareaPal[i], 
-       ylim = c(0,500),
+       ylim = c(0,50000),
        xlim = c(0,70),
        xlab = "Age", 
        ylab = 'Numbers')
-  for(y in 2:2){
-    lines(N_yais_beg[y,,1,1],  col = subareaPal[i], lwd =2)
+  box(which = 'plot', lty = 'solid', 
+      col = subareaPal[i],
+      lwd = 2)
+  for (y in 2:25) {
+    lines(N_yais_beg[y, , 1, 1],
+          # lty = c(2:6)[y],
+          col = gray.colors(25, start = 0.1, end = 0.9)[y],
+          lwd = 2)
   }
+
 }
 dev.off()
 
-
-pSSByi <- SSB_yi[1:52,] %>%
+## SSB y ----
+pSSByi <- SSB_yi %>%
   data.frame() %>%
-  mutate('Yr' = 1:52) %>%
+  mutate('Yr' = 1:nrow(SSB_yi)) %>%
   reshape2::melt(id = c('Yr')) %>%
   ggplot(., aes(x = Yr, y = value, color = variable )) +
   scale_color_manual(values = subareaPal) +
-  geom_line(lwd = 2) + labs(x = 'Modeled Year',y = 'SSB', color = 'subarea') +
+  geom_line(lwd = 2) + 
+  labs(x = 'Modeled Year',y = 'SSB', color = 'subarea') +
   ggsidekick::theme_sleek() + theme( legend.position = c(0.8,0.8))
 
 pSSByk <- SSB_yk[1:nyear,] %>%
@@ -78,6 +92,7 @@ pSSByk <- SSB_yk[1:nyear,] %>%
   geom_line(lwd = 2) + labs(x = 'Modeled Year',y = 'SSB', color = 'stock') +
   ggsidekick::theme_sleek() + theme( legend.position = c(0.8,0.8))
 
+## R y ----
 pRyi <- R_yi %>% data.frame() %>%
   mutate('Yr' = 1:nrow(.)) %>%
   reshape2::melt(id = c('Yr')) %>%
@@ -97,7 +112,7 @@ pRyk <- R_yk %>% data.frame() %>%
   ggsidekick::theme_sleek()+ theme( legend.position = c(0.8,0.8))
 
 
-
+## SRR ----
 pSRR <- R_yk %>% 
   data.frame() %>%
   mutate('Yr' = 1:nrow(.))  %>%
@@ -109,14 +124,15 @@ pSRR <- R_yk %>%
               data.frame() %>%
               mutate('Yr' = 1:nrow(.))  %>%
               reshape2::melt(.,id = c('Yr')) %>%
-              mutate(SSByk = value)%>%
+              mutate(SSByk = value) %>%
               select(-value,-variable)) %>%
-  ggplot(., aes(x = SSByk, y = RYK, color = variable,group = Yr )) +
+  ggplot(., aes(x = SSByk, y = RYK, color = variable, group = Yr...1)) +
   scale_color_manual(values = demPal) +
   geom_point() +
   labs(x = 'SSB',y = 'Recruits #', color = 'stock') +
   ggsidekick::theme_sleek() +
   facet_wrap(~ variable, scales = 'free')
+
 ggsave(pSRR,
        file = here('figs',
                    "SRR.png"),
@@ -164,16 +180,16 @@ pLAA2M <- Length_yais_beg[,,2,2] %>% data.frame() %>%
 (pLAA1F  | pLAA1M) /
   (pLAA2F  | pLAA2M)
 
-## distributions
+## LAA dist ----
 pA1 <- list() ## for area 1
-for(y in 1:10){ #45:dim(LengthAge_alyi_beg)[3]){ ## loop years
+for(y in 1:5){ #45:dim(LengthAge_alyi_beg)[3]){ ## loop years
   a1 <- 
     LengthAge_alyis_beg[,,y,1,1] %>%
     melt() %>%
     group_by(Var2) %>% 
-    mutate(sumP = sum(value), pbin = value/sumP)
+    mutate(sumP = sum(value), ## total prob within A-L bin
+           pbin = value/sumP) ## relative prob within A-L bin
   
-
   ggplot(a1,aes( x = Var2, y = pbin, color = factor(Var1))) +
     # geom_histogram(stat ='identity', position = 'stack') +
     geom_line() + 
@@ -203,8 +219,8 @@ for(y in 1:10){ #45:dim(LengthAge_alyi_beg)[3]){ ## loop years
   rm(a1)
   # p[[i]] <- qplot(1:10,10:1,main=i)
 }
-png(here("figs","LAA_Dist_A1.png"), 
-    height = 10, width = 10, unit = 'in', res = 420)
+# png(here("figs","LAA_Dist_A1.png"), 
+#     height = 10, width = 10, unit = 'in', res = 420)
 do.call(gridExtra::grid.arrange,pA1)
 dev.off()
 
@@ -233,15 +249,15 @@ dev.off()
 ## catches post F tuning
 
 ## new option - by area weighted Fs
-catch_yf_predt <- data.frame(matrix(NA, nrow = tEnd, ncol = nfleets_fish))
-for(flt in 1:nfleets_fish){
-  for(y in 1:(tEnd-1)){
-  catch_yf_predt[y,flt] <- sum(catch_yfi_pred[y,flt,])
-  }
-}
+# catch_yf_predt <- data.frame(matrix(NA, nrow = tEnd, ncol = nfleets_fish))
+# for(flt in 1:nfleets_fish){
+#   for(y in 1:(tEnd-1)){
+#   catch_yf_predt[y,flt] <- sum(catch_yfi_pred[y,flt,])
+#   }
+# }
 
 ## old option -- fleet -specific
-catch_yf_predt <- data.frame(catch_yf_pred)[1:(tEnd-1),]
+
 
 ## comparing options
 # plot(Freal_yf[,2] ~ rowSums(F_area_yfi[,2,]), 
@@ -252,33 +268,35 @@ catch_yf_predt <- data.frame(catch_yf_pred)[1:(tEnd-1),]
 #      xlab = 'New Method sum of F x fleet x Area', ylab = 'old method F_fleet')
 # abline(0,1,col = 'red',add = TRUE)
 
+catch_yf_predt <- data.frame(catch_yf_pred)
 
-
-names(catch_yf_predt) <- paste('Fleet',1:3)
+# names(catch_yf_predt) <- paste('Fleet',1:3)
 
 catch_yf_predt <- catch_yf_predt %>%
-  mutate(Year = 1:nrow(catch_yf_predt)) %>%
+  mutate(Year = year) %>%
   melt(id = 'Year') %>%
   mutate(Type = 'PRED')
 
-catch_yf_obst<- catch_yf_obs[1:52,]%>%  data.frame() %>%select(-X1) 
-names(catch_yf_obst) <- paste('Fleet',1:3)
+catch_yf_obst<- catch_yf_obs[1:length(year),] %>%  data.frame() %>%select(-Year) 
+# names(catch_yf_obst) <- paste('Fleet',1:3)
 
 catch_yf_obst <- catch_yf_obst %>%
-  mutate(Year = 1:52) %>%
+  mutate(Year = year) %>%
   melt(id = 'Year') %>%
   ## convert CV to SD via CV = mean/sd
   mutate(Type = 'OBS', 
+         # Year = , ## use real years
          lci = value - 1.96*(0.1*value),
          uci = value + 1.96*(0.1*value)) 
 
 ggplot(data = catch_yf_obst, aes(x = Year, y = value, color = variable)) +
   geom_line(data = catch_yf_predt, lwd = 0.75) +
-  scale_color_manual(values = c('blue','blue1','blue2')) +
+  scale_color_manual(values = fishfltPal) +
   geom_point(pch = 1, fill = NA, col = 'black') +
   geom_errorbar(aes(ymin = lci, ymax = uci), col = 'black') +
-  theme_sleek() + theme(legend.position = 'none')+
-  labs(y = 'Catch (lbs)', color = 'Fishing Fleet') +
+  theme_sleek() + 
+  theme(legend.position = 'none')+
+  labs(y = 'Catch', color = 'Fishing Fleet') +
   facet_wrap(~variable, scales = "free_y")
 
 ggsave(last_plot(),

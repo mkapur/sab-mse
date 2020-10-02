76f51b49101befd741eb344f0f73eb019fe1e160
Maia Sosa Kapur <kapurm@uw.edu>
2020-10-02 16:52:19 -0700

    add third column with nsamp position

--- R/functions/load_data_OM.R
+++ R/functions/load_data_OM.R
@@ -159,10 +159,10 @@ load_data_OM <- function(nspace = 6,
     
     phi_ff_acomp <- matrix(0, nrow = nfleets_acomp, ncol = 2) ## indicates the position of acomp fleet
     rownames(phi_ff_acomp) <- fltnames_acomp
-    colnames(phi_ff_acomp) <- c('fsh_slx_pos','srv_slx_pos')
-    phi_ff_acomp[,1] <- c(0,1,-1,5,-1,-1,7,8)
-    phi_ff_acomp[,2] <- c(-1,-1,5,-1,6,7,-1,-1)
-    
+    colnames(phi_ff_acomp) <- c('fsh_slx_pos','srv_slx_pos',"nsamp_pos")
+    phi_ff_acomp[,1] <- c(0,1,-1,5,-1,-1,7,8) ## position in fishery selex (-1 means not applicable)
+    phi_ff_acomp[,2] <- c(-1,-1,5,-1,6,7,-1,-1) ## Pos in survey
+    phi_ff_acomp[,3] <- c(5:12) ## ordering for nsamp
     ## in the fish or selex surv, to be selected depending on the acomp flttype swtich
     
     

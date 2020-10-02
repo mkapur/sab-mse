1a6626e31a20ae2d59872dc86b5bd6d0c229c088
Maia Sosa Kapur <kapurm@uw.edu>
2020-10-02 10:16:22 -0700

    functional case zero with a < nage; eigen error on case 1

--- TMB/shire.cpp
+++ TMB/shire.cpp
@@ -899,30 +899,30 @@ Type objective_function<Type>::operator() ()
           for(int s=0;s<2;s++){
             switch(acomp_flt_type(acomp_flt)){
             case 0: //commercial fleet, use fish selex. note fixW and fixE are also the first two in fsh slx
-              comm_acomp_yafs_pred(y,a-1,acomp_flt,s) +=
+              comm_acomp_yafs_pred(y,a,acomp_flt,s) +=
                 acomp_yaf_temp(y,a,acomp_flt)*
                 fsh_slx_yafs(y,a,acomp_flt,s)*
                 phi_if_fish(acomp_flt,i)*
                 N_yais_mid(y,a,i,s)/Nsamp_acomp_yf(y,acomp_flt);
               break;
-              // case 1:  // survey fleets. the selex for these start in position 5
-              //   if(selType_surv(acomp_flt+4) == 0){
-              //     surv_acomp_yafs_pred(y,a,acomp_flt,s) +=
-              //       acomp_yaf_temp(y,a,acomp_flt)*
-              //       srv_slx_yafs(y,a,acomp_flt+4) *
-              //       phi_if_surv(acomp_flt,i)*
-              //       N_yais_mid(y,a+1,i)/Nsamp_acomp_yf(y,acomp_flt);
-              //   } else{
-              // for(int l=1;l<(LBins);l++){
-              //   surv_acomp_yafs_pred(y,nage-1,acomp_flt) +=
-              //     acomp_yaf_temp(y,a,acomp_flt)*
-              //     srv_slx_yafs(y,l,acomp_flt+4) *
-              //     phi_if_surv(acomp_flt,i)*
-              //     mla_yais(y,a,i,s)*
-              //     N_yais_mid(y,a+1,i)/
-              //       Nsamp_acomp_yf(y,acomp_flt);
-              // } // end lbins
-              // } // end else
+            // case 1:  // survey fleets. the selex for these start in position 5
+            //   if(selType_surv(acomp_flt+4) == 0){
+            //     surv_acomp_yafs_pred(y,a,acomp_flt,s) +=
+            //       acomp_yaf_temp(y,a,acomp_flt)*
+            //       srv_slx_yafs(y,a,acomp_flt+4) *
+            //       phi_if_surv(acomp_flt,i)*
+            //       N_yais_mid(y,a+1,i)/Nsamp_acomp_yf(y,acomp_flt);
+            //   } else{
+            //     for(int l=1;l<(LBins);l++){
+            //       surv_acomp_yafs_pred(y,nage-1,acomp_flt) +=
+            //         acomp_yaf_temp(y,a,acomp_flt)*
+            //         srv_slx_yafs(y,l,acomp_flt+4) *
+            //         phi_if_surv(acomp_flt,i)*
+            //         mla_yais(y,a,i,s)*
+            //         N_yais_mid(y,a+1,i)/
+            //           Nsamp_acomp_yf(y,acomp_flt);
+            //     } // end lbins
+            //   } // end else
               break;
             } // end acomp fleet type
           } // end sex

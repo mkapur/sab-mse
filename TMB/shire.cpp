639f4a426b342c8fd9146148ef7cb2f970d8cc17
Maia Sosa Kapur <kapurm@uw.edu>
2020-10-05 13:02:01 -0700

    functional acomp pred on age sel fish

--- TMB/shire.cpp
+++ TMB/shire.cpp
@@ -963,14 +963,20 @@ Type objective_function<Type>::operator() ()
             case 0: // fish fleets
               switch(selType_fish(phi_ff_acomp(acomp_flt,0))){
               case 0: // age sel fish fleet
-                comm_acomp_yafs_pred(y,a,phi_ff_acomp(acomp_flt,3),s) += Nsamp_acomp_yf(y,phi_ff_acomp(acomp_flt,2))*
+                comm_acomp_yafs_pred(y,a,phi_ff_acomp(acomp_flt,3),s) +=
                 acomp_yaf_temp(y,a,acomp_flt)*
-                fsh_slx_yafs(y,a,acomp_flt,s);//*
-                // phi_if_fish(acomp_flt,i)*
-                // N_yais_mid(y,a,i,s)/  Nsamp_acomp_yf(y,phi_ff_acomp(acomp_flt,2));
+                fsh_slx_yafs(y,a,acomp_flt,s)*
+                phi_if_acomp(acomp_flt,i)*
+                N_yais_mid(y,a,i,s)/  Nsamp_acomp_yf(y,phi_ff_acomp(acomp_flt,2));
                 break;
               case 1: // len sel fish fleet
                 for(int l=0;l< LBins;l++){
+                  // comm_acomp_yafs_pred(y,a,phi_ff_acomp(acomp_flt,3),s) +=
+                  //   acomp_yaf_temp(y,a,acomp_flt)*
+                  //   fsh_slx_yafs(y,l,phi_ff_acomp(acomp_flt,0),s)*
+                  //   phi_if_acomp(acomp_flt,i)*
+                  //   LengthAge_alyis_mid(a,l,y,i,s)*
+                  //   N_yais_mid(y,a,i,s)/  Nsamp_acomp_yf(y,phi_ff_acomp(acomp_flt,2));
                 } // end LBins
                 break;
               } //end selType switch for comms

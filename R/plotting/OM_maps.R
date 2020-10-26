fcc3af6e9d1a2c8076130d7afb03a70f7d3467e4
Maia Sosa Kapur <kapurm@uw.edu>
2020-10-26 13:08:31 -0700

    fix maps for presentation with new inames
    OH was confused about subarea naming syntax; I've updated this to be alpha = mgmt, numeric = stock.

--- R/plotting/OM_maps.R
+++ R/plotting/OM_maps.R
@@ -53,19 +53,19 @@ ggplot(data = regions) +
   
   ## show demography from growth paper
   ##R3
-  # geom_sf(data =   st_union(x=clips[[2]],y=clips[[3]])  ,
-# fill = NA, lwd = 1.1,color = 'white', linetype = 'dashed') +
-# ##r2
-# geom_sf(data =   st_union(x=clips[[4]],y=clips[[5]])  ,
-# fill = NA, lwd = 1.1,  color = 'white', linetype = 'dashed') +
+geom_sf(data =   st_union(x=clips[[2]],y=clips[[3]])  ,
+fill = NA, lwd = 1.1,color = 'white', linetype = 'dotted') +
+##r2
+geom_sf(data =   st_union(x=clips[[4]],y=clips[[5]])  ,
+fill = NA, lwd = 1.1,  color = 'white', linetype = 'dotted') +
 
 ## panmictic fill
-geom_sf(data = clips[[1]], fill = 'grey44', alpha = 0.9, color = NA) +
-  geom_sf(data =  clips[[2]], fill = 'grey44', alpha = 0.9, color = NA) +
-  geom_sf(data = clips[[3]], fill = 'grey44', alpha = 0.9, color = NA) +
-  geom_sf(data = clips[[4]], fill = 'grey44', alpha = 0.9, color = NA ) +
-  geom_sf(data = clips[[5]], fill = 'grey44', alpha = 0.9, color = NA) +
-  geom_sf(data = clips[[6]], fill = 'grey44', alpha = 0.9,color = NA) +
+# geom_sf(data = clips[[1]], fill = 'grey44', alpha = 0.9, color = NA) +
+#   geom_sf(data =  clips[[2]], fill = 'grey44', alpha = 0.9, color = NA) +
+#   geom_sf(data = clips[[3]], fill = 'grey44', alpha = 0.9, color = NA) +
+#   geom_sf(data = clips[[4]], fill = 'grey44', alpha = 0.9, color = NA ) +
+#   geom_sf(data = clips[[5]], fill = 'grey44', alpha = 0.9, color = NA) +
+#   geom_sf(data = clips[[6]], fill = 'grey44', alpha = 0.9,color = NA) +
   ## subarea fills
   # geom_sf(data = clips[[1]], fill = subareaPal[1], alpha = 0.9, color = NA) +
   # geom_sf(data =  clips[[2]], fill = subareaPal[2], alpha = 0.9, color = NA) +
@@ -75,12 +75,12 @@ geom_sf(data = clips[[1]], fill = 'grey44', alpha = 0.9, color = NA) +
   # geom_sf(data = clips[[6]], fill = subareaPal[6], alpha = 0.9,color = NA) +
   
   ## stock fills
-  # geom_sf(data = clips[[1]], fill = demPal[1], alpha = 0.9, color = NA) +
-  # geom_sf(data =  clips[[2]], fill = demPal[2], alpha = 0.9, color = NA) +
-  # geom_sf(data = clips[[3]], fill = demPal[2], alpha = 0.9, color = NA) +
-  # geom_sf(data = clips[[4]], fill = demPal[3], alpha = 0.9, color = NA ) +
-  # geom_sf(data = clips[[5]], fill = demPal[3], alpha = 0.9, color = NA) +
-  # geom_sf(data = clips[[6]], fill = demPal[4], alpha = 0.9,color = NA) +
+  geom_sf(data = clips[[1]], fill = demPal[1], alpha = 0.9, color = NA) +
+  geom_sf(data =  clips[[2]], fill = demPal[2], alpha = 0.9, color = NA) +
+  geom_sf(data = clips[[3]], fill = demPal[2], alpha = 0.9, color = NA) +
+  geom_sf(data = clips[[4]], fill = demPal[3], alpha = 0.9, color = NA ) +
+  geom_sf(data = clips[[5]], fill = demPal[3], alpha = 0.9, color = NA) +
+  geom_sf(data = clips[[6]], fill = demPal[4], alpha = 0.9,color = NA) +
   
 
   ## EEZ
@@ -111,19 +111,20 @@ geom_sf(data = clips[[1]], fill = 'grey44', alpha = 0.9, color = NA) +
   #   color = 'black') +
   
   ## OM Stock labels
-  # geom_label(data = data.frame(), aes( x = c(238, 233, 223,  200),
-  #                                      y = c(33, 48,  54, 53)),
-  #            label = list(c(1:4),
-  #                         c(paste0('Stock ',c("R1", "R2", "R3","R4"))),
-  #                       c("R1", "R2", "R3","R4"))[[1]] ,
-  #            fill = rev(demPal),
-  #            color = 'black') +
+  geom_label(data = data.frame(), aes( x = c(238, 233, 225,  200),
+                                       y = c(33, 48,  54, 53)),
+             size = 6,
+             label = list(c(1:4),
+                          c(paste0('Stock ',c("R1", "R2", "R3","R4"))),
+                        c("R1", "R2", "R3","R4"))[[3]] ,
+             fill = 'white',
+             color = rev(demPal)) +
   coord_sf(xlim = c(165, 245), ylim = c(26, 65)) +
   labs(x ="",y="")
 
 # Save
-ggsave(here::here("figs", "map-EEZ_strata_lab3.png"), 
-       width = 22, height = 10)
+ggsave(here::here("figs", "map-EEZ_stock_lab1.png"), 
+       width = 10, height = 8)
 
 ggsave(subalab | stocklab,  
        file = here::here("figs", "map-EEZ_strata_stock.png"),

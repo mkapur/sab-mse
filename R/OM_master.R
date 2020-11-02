8d3728a20b428644b653f3a1c6684065955ed9b5
Maia Sosa Kapur <kapurm@uw.edu>
2020-11-02 09:41:03 -0800

    testing with F on and survey comps

--- R/OM_master.R
+++ R/OM_master.R
@@ -11,7 +11,7 @@ library(r4ss)
 library(here)
 library(ggsidekick)
 dllUSE = c("shire_v2L",'shire_v2L_1')[2]
-compile(here("TMB",paste0(dllUSE,".cpp")))
+# compile(here("TMB",paste0(dllUSE,".cpp")))
 dyn.load(dynlib(here("TMB",dllUSE)))
 
 source(here("R","functions",'load_files_OM.R'))
@@ -21,12 +21,12 @@ df <- load_data_OM(nspace = 6, move = TRUE) ## data that works with OM
 df$v1 = 0.7;  df$Fmax = 1.5;
 # df$v1 = 0.65; df$Fmax = 1.15;
 df$niter = 20
-df$yRun =  30 #df$tEnd-1
-# df$mat_age <- rep(1e-3,df$nage)
+df$yRun =  25 #df$tEnd-1
+df$mat_age <- rep(5e-2,df$nage)
 df$selShape_fish[5:7] <-  -1 ## turn OFF all slx for BC
 
-omega_0ij_map <- matrix(NA, nrow = 6, ncol = 6) ## turn everything off
-# omega_0ij_map[1,] <- df$parms$omega_0ij[1,] ## estimate to/from C1 only
+omega_0ij_map <- matrix(NA, nrow = 6, ncol = 6) ## turn off age-zero movement estimation
+omega_0ij_map[1,] <- df$parms$omega_0ij[1,] ## estimate to/from C1 only
 
 mappy <- list(
   # logh_k = factor(rep(NA, 4)),
@@ -47,27 +47,25 @@ system.time(obj <- MakeADFun(df,
                  dll =dllUSE,
                  map = mappy, ## fix everything for testing eigen fails
                  checkParameterOrder = TRUE)) 
-array(exp(obj$par[names(obj$par)=='log_fsh_slx_pars']), dim = c(9,2,2),
-      dimnames = list(df$fltnames_fish))
-## up to 30s
-system.time(rep1 <- obj$report()) ## one off caclulation using start pars
-head(round(rep1$catch_yf_pred/df$catch_yf_obs[,2:10],2),df$yRun)
-colSums(rep1$N_0ais) ## should not be super small anywhere
-rep1$SSB_0i ## should not be small or negative
-rep1$SSB_yi[3,] ## should match SSB0 without fishing
-round(rep1$SSB_yi[1:df$yRun,]) ## should not be small or negative
-round(rep1$R_yi[1:df$yRun,])
-rowSums(rep1$N_yais_end[1:df$yRun,,,1])
+# array(exp(obj$par[names(obj$par)=='log_fsh_slx_pars']), dim = c(9,2,2),
+#       dimnames = list(df$fltnames_fish))
+# ## up to 30s
+# system.time(rep1 <- obj$report()) ## one off caclulation using start pars
+# head(round(rep1$catch_yf_pred/df$catch_yf_obs[,2:10],2),df$yRun)
+# colSums(rep1$N_0ais) ## should not be super small anywhere
+# rep1$SSB_0i ## should not be small or negative
+# rep1$SSB_yi[3,] ## should match SSB0 without fishing
+# round(rep1$SSB_yi[1:df$yRun,]) ## should not be small or negative
+# round(rep1$R_yi[1:df$yRun,])
+# rowSums(rep1$N_yais_end[1:df$yRun,,,1])
 
 # rep1$N_yais_beg[1:7,c(0:4,71),,1]
 # rep1$N_yais_mid[1:7,c(0:4,71),,1]
 # rep1$N_yais_end[1:7,c(0:4,71),,1]
-# likes <- rep1$ans_tot %>% matrix(., ncol = length(.)) %>% data.frame()
-# names(likes) = c("SDR","CATCH","SURVEY","SURVCOMP","CATCHCOMP","PRIORS")
-# likes
+
 source(here('R','functions','boundPars.R')) ## bound selex etc
-array(exp(upper[names(upper)=='log_fsh_slx_pars']), dim = c(9,2,2),
-      dimnames = list(df$fltnames_fish))
+# array(exp(upper[names(upper)=='log_fsh_slx_pars']), dim = c(9,2,2),
+      # dimnames = list(df$fltnames_fish))
 ## about 65s for 22 years
 ## 7 hours for 44 yrs
 ## 3hrs for all years with bounds (just catch like)
@@ -84,8 +82,8 @@ system.time(opt <-
               )$opt) ## estimate; can repreat for stability)
 # for (k in 1:2)  opt <- nlminb(obj$env$last.par.best, obj$fn, obj$gr) 
 best <- obj$env$last.par.best ## update object with the best parameters
-array(round(exp(best[names(best)=='log_fsh_slx_pars'])), dim = c(9,2,2),
-      dimnames = list(df$fltnames_fish))
+# array(round(exp(best[names(best)=='log_fsh_slx_pars'])), dim = c(9,2,2),
+#       dimnames = list(df$fltnames_fish))
 ## 81 s
 dat <- obj$report(par = best)
 head(round(dat$catch_yf_pred/df$catch_yf_obs[,2:10],2),df$yRun)
@@ -109,7 +107,7 @@ likes
 ## save everything and plot
 cppname = substr(dllUSE,7,nchar(dllUSE))
 writeOM(dat=dat,obj = obj, opt = opt, rep=rep, cppname =cppname,
-        runname = paste0("-",df$yRun,"y_",cppname,"_M=",df$mat_age[1],"_noF"))
+        runname = paste0("-",df$yRun,"y_",cppname,"_M=",df$mat_age[1],"_Fon_SurvOn"))
 
 opt2$par
 opt2$objective

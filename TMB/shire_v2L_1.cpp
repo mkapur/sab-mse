5cc52e7d331f1f9fb540fa9efe78c95f0735a57d
Maia Sosa Kapur <kapurm@uw.edu>
2020-11-03 09:52:56 -0800

    introduce Neqn and mult by R0 vector
    based on mtg with AEP, it was wrong to only use the first column of the solve(I-Mat2) object, because that implies all recruitment happens in area 1 (which explains why C1 was like e+108). Instead we do the full solve(I-Mat2)*R0, which needs to be setup to have a # first, followed by a bunch of zeroes, then a # again for the next area.

--- TMB/shire_v2L_1.cpp
+++ TMB/shire_v2L_1.cpp
@@ -36,7 +36,7 @@ Type objective_function<Type>::operator() ()
   
   // // DEMOGRAPHY //
   DATA_VECTOR(mat_age); // natural mortality at age
-  DATA_VECTOR(Mat3Inv); // solve(I - (X %*% (A %*% (S %*% (H %*% S)))))[,1]
+  DATA_MATRIX(Neqn); // solve(I - (X %*% (A %*% (S %*% (H %*% S)))))
   // // movement //
   DATA_ARRAY(omega_ais); // eigenvect of movement between subareas for ages > 0
   DATA_ARRAY(X_ijas); // prob trans between subareas at age
@@ -384,16 +384,18 @@ Type objective_function<Type>::operator() ()
   // matrix<Type> LinvN = LN.inverse(); // now LN is an object and we do inverse on it
   
   N_0ais.setZero();
-  // vector<Type> Mat3Inv = Mat3.inverse().col(0); // vector of dim nage*nspace
-
-    for(int i=0;i<(nspace);i++){
-      for(int s=0;s<nsex;s++){
-        N_0ais(0,i,s) = 0.5*R_0k(phi_ik2(i))*tau_ki(phi_ik2(i),i)*exp(epsilon_tau(i));
-        for(int a=1;a<(nage);a++){
-        N_0ais(a,i,s) = Mat3Inv(a+(i*nage));//*0.5*R_0k(phi_ik2(i))*tau_ki(phi_ik2(i),i);
-      }
-    }
-  }
+  // calc true Neqn given recruitment (matrix multiplication)
+  // new dims are subarea x age, note indexing [use block to subset?]
+  matrix<Type> NeqnR = (Neqn*R_0k);
+  
+  //   for(int i=0;i<(nspace);i++){
+  //     for(int s=0;s<nsex;s++){
+  //       N_0ais(0,i,s) = 0.5*R_0k(phi_ik2(i))*tau_ki(phi_ik2(i),i)*exp(epsilon_tau(i));
+  //       for(int a=1;a<(nage);a++){
+  //       N_0ais(a,i,s) = Mat3Inv(a+(i*nage));//*0.5*R_0k(phi_ik2(i))*tau_ki(phi_ik2(i),i);
+  //     }
+  //   }
+  // }
 
   //       N_0ais(0,i,s) = 0.5*R_0k(phi_ik2(i))*tau_ki(phi_ik2(i),i);
   //         for(int a=1;a<(nage-1);a++){ // we will fill recruits (a0) later

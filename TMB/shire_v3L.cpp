f06cb83feae6c31b21c1d1c7b9d3625ce1cb0b12
Maia Sosa Kapur <kapurm@uw.edu>
2020-11-30 12:38:16 -0800

    mortality age indexing and add multiplier
    i'm concerned that the setup within the hybrid method is now inconsistent with the instantaenous vibe, since we're tuning it using Z...

--- TMB/shire_v3L.cpp
+++ TMB/shire_v3L.cpp
@@ -487,7 +487,7 @@ Type objective_function<Type>::operator() ()
     // N- and Nominal Length - at-age for the middle of this year 
     for(int s=0;s<nsex;s++){
       for(int i=0;i<(nspace);i++){
-        N_yais_mid(y,0,i,s) = N_yais_beg(y,0,i,s)*exp(-mat_age(a)/2);
+        N_yais_mid(y,0,i,s) = N_yais_beg(y,0,i,s)*exp(-mat_age(0)/2);
         // linear growth below A4 as in synthesis
         // if(L1_yk(y,phi_ik2(i),s) < 3){
         //   lenstep = L1_yk(y,phi_ik2(i),s);
@@ -532,7 +532,7 @@ Type objective_function<Type>::operator() ()
             NCome += X_ijas(j,i,nage-1,s)*(N_yais_beg(y,nage-1,j,s) + N_yais_beg(y,nage-2,j,s));
           } // end i != j
         } // end subareas j
-        N_yais_mid(y,nage-1,i,s) =((1-pLeave)*N_yais_beg(y,nage-1,i,s) + NCome)*exp(-mat_age(a)/2);
+        N_yais_mid(y,nage-1,i,s) =((1-pLeave)*N_yais_beg(y,nage-1,i,s) + NCome)*exp(-mat_age(nage-1)/2);
         // plus group weighted average (we already have the numbers at age)
         // Length_yais_beg(y,nage-1,i,s) = (N_yais_beg(y,nage-2,i,s)*
         //   (Length_yais_beg(y,nage-2,i,s)+
@@ -802,7 +802,7 @@ Type objective_function<Type>::operator() ()
                     phi_if_fish(fish_flt, i)*
                     fsh_slx_yafs(y,a,fish_flt,s)*
                     F_area_yfi(y,fish_flt,i)*
-                    N_yais_mid(y,a,i,s)
+                    N_yais_mid(y,a,i,s)*
                     wtatlen_kab(phi_ik2(i),0)*
                     pow(mla_yais(y,a,i,s),wtatlen_kab(phi_ik2(i),1));
                 // std::cout << y << "\t" << fish_flt <<"\t catch_yaf_pred \t" <<    catch_yaf_pred(y,a,fish_flt)<< "\n";

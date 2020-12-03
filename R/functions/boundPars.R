fa5e946d36ea8b3e985588f66eafcbc3dbce7a3b
Maia Sosa Kapur <kapurm@uw.edu>
2020-12-03 13:21:36 -0800

    neable bounds on b, log q_f

--- R/functions/boundPars.R
+++ R/functions/boundPars.R
@@ -17,11 +17,11 @@ boundPars <- function(obj, r0_lower = 10, boundSlx = c('fsh','srv')){
   # lower[names(lower) == 'omega_0ij'] <- log(0.0001)
   # upper[names(upper) == 'omega_0ij'] <- log(0.999)
   # 
-  # lower[names(lower) == 'b'] <- 0
-  # upper[names(upper) == 'b'] <- 1
+  lower[names(lower) == 'b'] <- 0
+  upper[names(upper) == 'b'] <- 1
   
-  # lower[names(lower) == 'logq_f'] <- log(0.0001)
-  # upper[names(upper) == 'logq_f'] <- log(1)
+  lower[names(lower) == 'logq_f'] <- log(0.0001)
+  upper[names(upper) == 'logq_f'] <- log(1)
   ## bounds on fsh slx ----
   
   ## brute force par locations; if mappy mirrors first ak fleets,

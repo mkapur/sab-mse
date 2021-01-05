4b5e06890c1f9ebc056cfc18d4cc9abd3ca6eb0c
Maia Sosa Kapur <kapurm@uw.edu>
2021-01-05 15:59:29 -0800

    non-exclusionary bounds on p1, p2
    some flts need p2 < p1
    if this takes too long or sucks can do a custom setup

--- R/functions/boundPars.R
+++ R/functions/boundPars.R
@@ -115,11 +115,11 @@ boundPars <- function(obj, r0_lower = 10, boundSlx = c('fsh','srv')){
       # array(1:length(obj$par[names(obj$par) == "log_srv_slx_pars"]), dim = c(8-nfixedfleets,2,1,2))
       seeddim <-  df$nfleets_surv+df$nfleets_acomp-4-nfixedfleets
       ## specific bounds on p1 and p2
-      lower[names(lower) == 'log_srv_slx_pars'][c(1:seeddim,(2*seeddim+1):(3*seeddim))] <- log(10) ## p1 
-      lower[names(lower) == 'log_srv_slx_pars'][c((seeddim+1):(2*seeddim),(3*seeddim+1):(4*seeddim))] <- log(25) ## p2
+      lower[names(lower) == 'log_srv_slx_pars'][c(1:seeddim,(2*seeddim+1):(3*seeddim))] <- log(30) ## p1 
+      lower[names(lower) == 'log_srv_slx_pars'][c((seeddim+1):(2*seeddim),(3*seeddim+1):(4*seeddim))] <- log(80) ## p2
       
-      upper[names(upper) == 'log_srv_slx_pars'][c(1:seeddim,(2*seeddim+1):(3*seeddim))] <- log(50) ## p1 3.91202301
-      upper[names(upper) == 'log_srv_slx_pars'][c((seeddim+1):(2*seeddim),(3*seeddim+1):(4*seeddim))] <- log(75) ## p2 3.91202301
+      upper[names(upper) == 'log_srv_slx_pars'][c(1:seeddim,(2*seeddim+1):(3*seeddim))] <- log(30) ## p1 3.91202301
+      upper[names(upper) == 'log_srv_slx_pars'][c((seeddim+1):(2*seeddim),(3*seeddim+1):(4*seeddim))] <- log(80) ## p2 3.91202301
       
       ## just rational bounds
       # lower[names(lower) == 'log_srv_slx_pars'] <- 0
@@ -129,14 +129,19 @@ boundPars <- function(obj, r0_lower = 10, boundSlx = c('fsh','srv')){
     } else   if(length(grep("log_srv_slx_pars", names(mappy)))  == 0){
       
       seeddim = df$nfleets_surv+df$nfleets_acomp-4
-      lower[names(lower) == 'log_srv_slx_pars'][c(1:seeddim,(2*seeddim+1):(3*seeddim))] <- log(10) ## p1
-      lower[names(lower) == 'log_srv_slx_pars'][c((seeddim+1):(2*seeddim),(3*seeddim+1):(4*seeddim))] <- log(25) ## p2
+      lower[names(lower) == 'log_srv_slx_pars'][c(1:seeddim,(2*seeddim+1):(3*seeddim))] <- log(30) ## p1
+      lower[names(lower) == 'log_srv_slx_pars'][c((seeddim+1):(2*seeddim),(3*seeddim+1):(4*seeddim))] <- log(70) ## p2
 
-      upper[names(upper) == 'log_srv_slx_pars'][c(1:seeddim,(2*seeddim+1):(3*seeddim))] <- log(50) ## p1
-      upper[names(upper) == 'log_srv_slx_pars'][c((seeddim+1):(2*seeddim),(3*seeddim+1):(4*seeddim))] <- log(75) ## p2
+      upper[names(upper) == 'log_srv_slx_pars'][c(1:seeddim,(2*seeddim+1):(3*seeddim))] <- log(30) ## p1
+      upper[names(upper) == 'log_srv_slx_pars'][c((seeddim+1):(2*seeddim),(3*seeddim+1):(4*seeddim))] <- log(70) ## p2
+      
       
       
       ## custom bounds
+      # upper[names(upper) == 'log_srv_slx_pars'][c(3,19)] <- log(75) ## BCEARLY P1
+      # upper[names(upper) == 'log_srv_slx_pars'][c(11,27)] <- log(75) ## BCEARLY P2
+      # lower[names(lower) == 'log_srv_slx_pars'][c(11,27)] <- log(10) ## BCEARLY P2
+      
       # upper[names(upper) == 'log_srv_slx_pars'][c(2,18)] <- log(80) ## AKVASTE P1
       # upper[names(upper) == 'log_srv_slx_pars'][c(3,19)] <- log(80) ## BCEARLY P1
       
@@ -144,6 +149,7 @@ boundPars <- function(obj, r0_lower = 10, boundSlx = c('fsh','srv')){
       # upper[names(upper) == 'log_srv_slx_pars'][20] <- log(80) ## BCVAST P1 MALE only
       # upper[names(upper) == 'log_srv_slx_pars'][c(11,27)] <- log(85) ## BCEARLY P2
       
+      # upper[names(upper) == 'log_srv_slx_pars'][c(11,27)] <- log(85) ## BCEARLY P2
       
       # lower[names(lower) == 'log_srv_slx_pars'] <- 0
       # upper[names(upper) == 'log_srv_slx_pars']<- log(80)

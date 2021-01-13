a6867570c419533246d6fbc9d9ab5cf90965650d
Maia Sosa Kapur <kapurm@uw.edu>
2021-01-13 15:23:20 -0800

    upper and lower bounds on log SDR

--- R/functions/boundPars.R
+++ R/functions/boundPars.R
@@ -18,15 +18,18 @@ boundPars <- function(obj, r0_lower = 10, boundSlx = c('fsh','srv')){
   
   lower[names(lower) == 'mort_k'] <- 0 ## duh
   upper[names(upper) == 'mort_k'] <- 1
-  lower[names(lower) == 'logSDR'] <- log(0.0001)
+  
+  lower[names(lower) == 'logSDR'] <- 0
+  upper[names(upper) == 'logSDR'] <- 10
+  
   lower[names(lower) == 'omega_0ij'] <- log(0.0001)
   upper[names(upper) == 'omega_0ij'] <- log(0.999)
   
   lower[names(lower) == 'b_y'] <- 0
   upper[names(upper) == 'b_y'] <- 1
-  
+
   lower[names(lower) == 'logq_f'] <- 0
-  # upper[names(upper) == 'logq_f'] <- log(1)
+  upper[names(upper) == 'logq_f'] <- log(1)
   ## bounds on fsh slx ----
   
   ## brute force par locations; if mappy mirrors first ak fleets,

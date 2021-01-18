fe14302854150c674974732e6462659ab71be932
Maia Sosa Kapur <kapurm@uw.edu>
2021-01-18 15:44:08 -0800

    enable droplist from srv fleet names

--- R/OM_master.R
+++ R/OM_master.R
@@ -17,21 +17,30 @@ dyn.load(dynlib(here("TMB",dllUSE)))
 
 source(here("R","functions",'load_files_OM.R'))
 df <- load_data_OM(nspace = 6, 
-                   move = TRUE) ## data that works with OM
+                   move = TRUE,
+                   b_y_max = 0.109) ## data that works with OM
 df$surv_yf_obs[df$surv_yf_obs >0] <-  df$surv_yf_obs[df$surv_yf_obs >0]*1000
 df$yRun <- df$tEnd ## number of years to run model
 df$parms$mort_k <- c(0.2,0.2,0.2,0.2)
 df$Neqn <- buildNeqn(df)
-
+# df$parms$b_y <- rep(0,df$tEnd) ## no ramp right now.
 mappy <-
   buildMap(toFix =  c("omega_0ij",
-                      # "b_y",
+                      "logh_k",
+                      "logSDR",
+                      # "tildeR_yk",
+                      "b_y",
                       "epsilon_tau",
+                      "logpi_acomp",
                       "log_fsh_slx_pars",
                       "log_srv_slx_pars",
                     "mort_k"),
-           fixFlt = c("all_fsh", "AK_GOA_SURV", "BC_StRS", "BC_SS"))
-mappy$b_y <- factor(c(1,rep(NA,df$yRun-1))) ## enable estimation of year 1 b_y
+           fixFlt = c("all_fsh",
+                    c( paste0(c(as.character(df$fltnames_surv),as.character(df$fltnames_acomp[c(2,4,5)])))[-1] )))
+# mappy$logh_k <- factor(c(NA,NA,2,3)) ##  fix WC regs
+# mappy$b_y <- factor(c(1,rep(NA,59))) ## enable estimation of year 1 b_y ## consider mirroring for these guys
+# mappy$tildeR_yk <- factor(sort(rep(1:(length(mappy$tildeR_yk)/df$nstocks), each = df$nstocks))) ## make each area x year mirrored
+
 
 array(mappy$log_fsh_slx_pars, dim = c(df$nfleets_fish,2,max(df$fsh_blks_size),2),
       dimnames = dimnames(df$parms$log_fsh_slx_pars))
@@ -41,6 +50,7 @@ array(mappy$log_srv_slx_pars, dim = c(df$nfleets_surv+df$nfleets_acomp-4,2,max(d
 system.time(obj <- MakeADFun(df,
                  parameters = df$parms,
                  dll = dllUSE,
+                 # random = "tildeR_y",
                  map = mappy, ## fix everything for testing eigen fails
                  checkParameterOrder = TRUE)) 
 
@@ -56,7 +66,7 @@ rep1$Length_yais_end[4,c(1,2,65:71),1,1]
 
 bounds <- boundPars(obj,
                     r0_lower = 0, 
-                    boundSlx = c(NA,'fsh','srv')[3])
+                    boundSlx = c(NA,'fsh','srv')[2:3])
 
 
 ## inspect survey bounds in proper format
@@ -78,7 +88,7 @@ system.time(opt <-
 best <- obj$env$last.par.best ## update object with the best parameters
 dat <- obj$report(par = best)
 dat$surv_yf_pred/df$surv_yf_obs
-dat$catch_yf_pred_total/df$catch_yf_obs[,2:ncol(df$catch_yf_obs)]
+# dat$catch_yf_pred_total/df$catch_yf_obs[,2:ncol(df$catch_yf_obs)]
 ## save everything and plot
 cppname = substr(dllUSE,7,nchar(dllUSE))
 writeOM(justPlots = FALSE,
@@ -88,19 +98,22 @@ writeOM(justPlots = FALSE,
         rep=rep, 
         cppname =cppname, 
         mappy = mappy,
-        runname = paste0("-",df$yRun,"y_",cppname,
-                         "_b_y0on",
-                         "_epstauoff"))
+        runname = paste0("-",df$yRun,"y_",
+                         cppname,
+                         "_tildeR_yON",
+                         "_onlyAKVASTWest",
+                         "_B_y0_off"))
 
 
 # system.time(rep <- sdreport(obj, par = best)) ## re-run & return values at best pars
-
-array(exp(best[names(best)== "log_srv_slx_pars"]), dim = c(5,2,1,2))
+array(exp(best[names(best)== "log_fsh_slx_pars"]), dim = c(2,2,1,2))
+array(exp(best[names(best)== "log_srv_slx_pars"]), dim = c(5,2,3,2))
 steep <- exp(best[names(best) == 'logh_k']); names(steep) <- paste0("h","_R",1:4);steep
 logR_0 <- best[names(best) == 'logR_0k'];names(logR_0) <- paste0("logR_0","_R",1:4);logR_0
 epstau <- exp(best[names(best) == 'epsilon_tau']); names(epstau) <- paste0("epstau_",
                                                                       c('C1','C2','B2','B3','A3','A4'));epstau
-by <- exp(best[names(best) == 'b_y']); names(steep) <- paste0("b_y",1:length((best[names(best) == 'b_y']));steep
+by <- round(best[names(best) == 'b_y'],3); 
+names(by) <- paste0("b_y",1:length((best[names(best) == 'b_y'])));by 
 
 likes <- dat$ans_tot %>% matrix(., ncol = length(.)) %>% data.frame()
 names(likes) = c("SDR","CATCH","SURVEY","SURVCOMP","CATCHCOMP","PRIORS")

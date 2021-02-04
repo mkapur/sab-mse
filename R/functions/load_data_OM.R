fa55cb3059ab256c5c88e7a8c7b47b0fcb3e5590
Maia Sosa Kapur <kapurm@uw.edu>
2021-02-03 16:49:16 -0800

    disable BC vast before 2003 as experimental measure
    because this was run just using the data before 2003, it is really uncertain and does weird things beforehand. this is experimental; other ideas include
    -update VAST analysis to use the BC_STD values 1991-2009 within standardization
    -use the straight up strs values as they were (latter years quite similar to vast), in lieu of vast for bc at all

--- R/functions/load_data_OM.R
+++ R/functions/load_data_OM.R
@@ -122,14 +122,18 @@ load_data_OM <- function(nspace = 6,
   # survey_err <- read.csv(here("input","input_data",'OM_indices_sigma_BaseQ=GOA_Late.csv'))
   
   survey <- read.csv(here("input","input_data",'OM_indices_BaseQ=WCGBTS.csv'))
+  ## disable BC-VAST before 2003 - experimental
+  survey$BC_VAST[1:43] <- NA
   survey[is.na(survey)] <- -1.0## flag for numeric TMB checks
+  
+  
   # survey[,"BC_EARLY"] <-  survey[,"BC_EARLY"] +0.0111 ## flag for numeric TMB checks
   # survey <- round(survey,  1)
   survey_err <- read.csv(here("input","input_data",'OM_indices_sigma_BaseQ=WCGBTS.csv'))
   
   
   ## Comps ----
-  ## Len comps [these are arrays by fleet]
+  #* Len comps [these are arrays by fleet]
   load(here("input","input_data",'OM_lencomps_female.rdata'))
   load(here("input","input_data",'OM_lencomps_male.rdata'))
   

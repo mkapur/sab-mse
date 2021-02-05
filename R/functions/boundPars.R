e63f24af07941b7a87f7cf9886d78a9774e0c6f5
Maia Sosa Kapur <kapurm@uw.edu>
2021-02-05 10:55:19 -0800

    modify all bounds to look like earlier runs
    oddly including the bc nominal fleets resulted in better fits to those fleets, but it's been so long since we estimated everyone that the bounds were too large for all others. fortunately estimation seems to be quite fast.

--- R/functions/boundPars.R
+++ R/functions/boundPars.R
@@ -142,17 +142,17 @@ boundPars <- function(obj, r0_lower = 10, boundSlx = c('fsh','srv')){
       #                       c('block',1:max(df$srv_blks_size)),c('Fem','Mal')))
       
       if(all(!is.na(map_srvslx['AK_VAST_W',,1,]))){
-        lwr.temp['AK_VAST_W',"p1",1:df$srv_blks_size[,'AK_VAST_W'],] <- log(30); 
-        upr.temp['AK_VAST_W',"p1",1:df$srv_blks_size[,'AK_VAST_W'],] <- log(40); 
+        lwr.temp['AK_VAST_W',"p1",1:df$srv_blks_size[,'AK_VAST_W'],] <- log(35); 
+        upr.temp['AK_VAST_W',"p1",1:df$srv_blks_size[,'AK_VAST_W'],] <- log(41); 
         
-        lwr.temp['AK_VAST_W',"p2",1:df$srv_blks_size[,'AK_VAST_W'],] <- log(40); 
-        upr.temp['AK_VAST_W',"p2",1:df$srv_blks_size[,'AK_VAST_W'],] <- log(70)
+        lwr.temp['AK_VAST_W',"p2",1:df$srv_blks_size[,'AK_VAST_W'],] <- log(44); 
+        upr.temp['AK_VAST_W',"p2",1:df$srv_blks_size[,'AK_VAST_W'],] <- log(55)
       }
       if(all(!is.na(map_srvslx['AK_VAST_E',,1,]))){
-        lwr.temp['AK_VAST_E',"p1",1:df$srv_blks_size[,'AK_VAST_E'],] <- log(20);
-        upr.temp['AK_VAST_E',"p1",1:df$srv_blks_size[,'AK_VAST_E'],] <- log(70)
+        lwr.temp['AK_VAST_E',"p1",1:df$srv_blks_size[,'AK_VAST_E'],] <- log(22);
+        upr.temp['AK_VAST_E',"p1",1:df$srv_blks_size[,'AK_VAST_E'],] <- log(41)
         
-        lwr.temp['AK_VAST_E',"p2",1:df$srv_blks_size[,'AK_VAST_E'],] <- log(20); 
+        lwr.temp['AK_VAST_E',"p2",1:df$srv_blks_size[,'AK_VAST_E'],] <- log(50); 
         upr.temp['AK_VAST_E',"p2",1:df$srv_blks_size[,'AK_VAST_E'],] <- log(70)
       }
       
@@ -187,11 +187,11 @@ boundPars <- function(obj, r0_lower = 10, boundSlx = c('fsh','srv')){
         }
       }
       if(all(!is.na(map_srvslx['WC_VAST',,1,]))){
-        lwr.temp['WC_VAST',"p1",1:df$srv_blks_size[,'WC_VAST'],] <- log(20);
-        upr.temp['WC_VAST',"p1",1:df$srv_blks_size[,'WC_VAST'],] <- log(70)
+        lwr.temp['WC_VAST',"p1",1:df$srv_blks_size[,'WC_VAST'],] <- log(30);
+        upr.temp['WC_VAST',"p1",1:df$srv_blks_size[,'WC_VAST'],] <- log(65)
         
-        lwr.temp['WC_VAST',"p2",1:df$srv_blks_size[,'WC_VAST'],'Mal'] <- log(50);  
-        lwr.temp['WC_VAST',"p2",1:df$srv_blks_size[,'WC_VAST'],'Fem'] <- log(45);
+        lwr.temp['WC_VAST',"p2",1:df$srv_blks_size[,'WC_VAST'],'Mal'] <- log(40);  
+        lwr.temp['WC_VAST',"p2",1:df$srv_blks_size[,'WC_VAST'],'Fem'] <- log(40);
         upr.temp['WC_VAST',"p2",1:df$srv_blks_size[,'WC_VAST'],] <- log(70)
       }
       lower[names(lower) == 'log_srv_slx_pars'] <- lwr.temp[!is.na(lwr.temp)]

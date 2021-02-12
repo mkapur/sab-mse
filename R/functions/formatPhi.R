8d0dd92e811fbbc8940ac9b9653660db0d01b6e3
Maia Sosa Kapur <kapurm@uw.edu>
2021-02-11 16:58:23 -0800

    incl phi_ik2

--- R/functions/formatPhi.R
+++ R/functions/formatPhi.R
@@ -98,7 +98,7 @@ format_phi <- function(df = NULL, no_strata = 3){
     rownames(phi_im) <- unique(rev(spmat$subarea))
     phi_im[1:2,1] <- phi_im[3:4,2] <- phi_im[5:6,3] <- 1
     
-    ## phi_ik
+    ## phi_ki
     phi_ki <-  matrix(0, ncol = no_strata, nrow = nstocks) ## nesting of subareas within stocks, for recruitment purposes
     rownames(phi_ki) <- rev(unique(spmat$stock))
     colnames(phi_ki) <- unique(rev(spmat$subarea))
@@ -109,12 +109,15 @@ format_phi <- function(df = NULL, no_strata = 3){
       phi_ki[i,which(colnames(phi_ki)  %in% SA)] <- 1
     }
    
+    phi_ik2 <- matrix(apply(phi_ki,2, function(x)which(x == 1))-1) ## a vector for par subsetting, the columns are subareas
+    
     ## phi_ij [eq 6]
-    phi_ij <-  matrix(1, ncol = no_strata, nrow = no_strata) ## 0 indicates  subareas comprise THE SAME stock
-    rownames(phi_ij) = colnames(phi_ij) = rev(spmat$subarea)
-    diag(phi_ij) <- phi_ij[2:3,2:3] <- phi_ij[4:5,4:5] <- 0
-    
-    
+    phi_ij <-  matrix(1, ncol = no_strata, nrow = no_strata) ## 0 indicates subareas comprise THE SAME stock
+    rownames(phi_ij) = colnames(phi_ij) = unique(rev(spmat$subarea))
+    if(no_strata == 4) phi_ij <- 1 ## not convinced what to do for n = 3
+    if(no_strata == 4) diag(phi_ij) <- 0 
+    if(no_strata == 6) diag(phi_ij) <- phi_ij[2:3,2:3] <- phi_ij[4:5,4:5] <- 0
+
     ## phi_fm
     phi_fm <- matrix(0, nrow = nfleets_fish, ncol = 3)
     rownames(phi_fm) = names(catch)[2:ncol(catch)]
@@ -145,9 +148,7 @@ format_phi <- function(df = NULL, no_strata = 3){
     }
     # phi_fm_acomp[1:3,3] <- phi_fm_acomp[4:6,2]  <- phi_fm_acomp[7:8,1]  <- 1
     phi_fm_acomp2 <- matrix(apply(phi_fm_acomp,1, function(x)which(x == 1))-1) ## a vector for par subsetting, the columns are survey fleets
-    
-    
-    
+
     phi_lcomp_fm <- matrix(0, nrow = nfleets_lcomp, ncol = 3)
     rownames(phi_lcomp_fm) = fltnames_lcomp
     colnames(phi_lcomp_fm) = rev(unique(spmat$mgmt))
@@ -165,15 +166,27 @@ format_phi <- function(df = NULL, no_strata = 3){
     ## tau_ki
     tau_ki <-  matrix(0, ncol = no_strata, nrow = nstocks) ## nesting of subareas within stocks, for recruitment purposes
     rownames(tau_ki) <- rev(unique(spmat$stock))
-    colnames(tau_ki) <- rev(spmat$subarea)
-    tau_ki[1,1] <-   tau_ki[4,6]  <- 1 ## 100% of recruitment in stock
-    tau_ki[2,2:3] <-  tau_ki[3,5:4] <-  c(0.75,0.25) ## A2 and C1 are larger
-   }
+    colnames(tau_ki) <- unique(rev(spmat$subarea))
+    if(no_strata == 4){
+      diag(tau_ki) <- 1 ## all self-seeding
+    }
+    if(no_strata == 3){
+     tau_ki['R1','M1'] <- 1## all s 36 in a1
+     tau_ki['R2','M2'] <-  0.75; tau_ki['R3','M2'] <- 0.25 ## most of R2 from CC
+     tau_ki['R2','M2'] <-  0.75; tau_ki['R3','M2'] <- 0.25 ## most of R2 from CC
+     tau_ki['R3','M3'] <- 0.25 ; tau_ki['R4','M3'] <- 0.75 ## most of R4 from AK
+    }
+    if(no_strata == 6){
+      tau_ki[1,1] <-   tau_ki[4,6]  <- 1 ## 100% of recruitment in stock
+      tau_ki[2,2:3] <-  tau_ki[3,5:4] <-  c(0.75,0.25) ## A2 and C1 are larger
+    }
+   
+    return(list(phi_if_surv,phi_if_fish,phi_if_acomp,phi_ff_acomp,
+                phi_ki,phi_im,phi_ik2,phi_ij,phi_fm,tau_ki,phi_fm_acomp,
+                phi_fm_acomp2,phi_lcomp_fm))
+} ## end func
 
-  }
-  
 
-}
 ## this can take either a list of replicates or single value
 plotSimData<- function(simdat, new_strata =6){
   savedir <- here('output','om_simulations',paste0(Sys.time(),"_n=", ifelse(is.null(dim(simdat)[2]),1,dim(simdat)[2])))

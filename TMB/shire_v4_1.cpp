e33bae7c29987ae90e03da84ae34d1db4566e85c
Maia Sosa Kapur <kapurm@uw.edu>
2021-03-31 13:44:53 -0700

    remove scalar on nsamp

--- TMB/shire_v4_1.cpp
+++ TMB/shire_v4_1.cpp
@@ -926,13 +926,13 @@ Type objective_function<Type>::operator() ()
               switch(selType_fish(phi_ff_acomp(acomp_flt,0))){
               case 0: // age sel fish fleet
                 Nsamp_acomp_yf(y,phi_ff_acomp(acomp_flt,2)) +=
-                  (1/1000)*fsh_slx_yafs(y,a,phi_ff_acomp(acomp_flt,0),s)*
+                  fsh_slx_yafs(y,a,phi_ff_acomp(acomp_flt,0),s)*
                   phi_if_acomp(acomp_flt,i)*
                   N_yais_mid(y,a,i,s);
                 break;
               case 1: // len sel fish fleet
                 Nsamp_acomp_yf(y,phi_ff_acomp(acomp_flt,2)) +=
-                  (1/1000)*fsh_slx_yafs(y,mla_yais(y,a,i,s),phi_ff_acomp(acomp_flt,0),s)*
+                  fsh_slx_yafs(y,mla_yais(y,a,i,s),phi_ff_acomp(acomp_flt,0),s)*
                   phi_if_acomp(acomp_flt,i)*
                   // mla_yais(y,a,i,s)*
                   N_yais_mid(y,a,i,s);
@@ -941,12 +941,12 @@ Type objective_function<Type>::operator() ()
             }else{
               if(selType_surv(phi_ff_acomp(acomp_flt,1)) == 0){
                 Nsamp_acomp_yf(y,phi_ff_acomp(acomp_flt,2)) +=
-                  (1/1000)*srv_slx_yafs(y,a,phi_ff_acomp(acomp_flt,1),s)*
+                  srv_slx_yafs(y,a,phi_ff_acomp(acomp_flt,1),s)*
                   phi_if_acomp(acomp_flt,i)*
                   N_yais_mid(y,a,i,s);
               }else{
                 Nsamp_acomp_yf(y,phi_ff_acomp(acomp_flt,2)) +=
-                  (1/1000)*srv_slx_yafs(y,mla_yais(y,a,i,s),phi_ff_acomp(acomp_flt,1),s)*
+                  srv_slx_yafs(y,mla_yais(y,a,i,s),phi_ff_acomp(acomp_flt,1),s)*
                   phi_if_acomp(acomp_flt,i)*
                   N_yais_mid(y,a,i,s);
               } // end selType switch for survs
@@ -1094,7 +1094,7 @@ Type objective_function<Type>::operator() ()
           } // end acomp flag
         } // end ages within dims
       } // end sex
-      // std::cout << y << "\t" << acomp_flt << "\t sum1 = " <<  sum1  << "\n";
+      std::cout << y << "\t" << acomp_flt << "\t sum1 = " <<  sum1  << "\n";
       // std::cout << y << "\t" << acomp_flt << "\t ans_catchcomp = " <<  ans_catchcomp  << "\n";
       // std::cout << y << "\t" << acomp_flt << "\t ans_survcomp = " <<  ans_survcomp  << "\n";
     } // end y in nyear
